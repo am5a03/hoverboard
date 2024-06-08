72be96589dd66a028fe83557b49d835f4dac4a8e
Chan <am5a03@gmail.com>
2024-06-07 18:40:53 -0700

    init commit

--- src/__mocks__/firebase.ts
+++ src/__mocks__/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseApp = initializeApp({
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: 'AIzaSyDC0_v9AWBXlHtDckPxZSQ5y0PXwgrcbaA',
+  authDomain: 'gdg-burnaby.firebaseapp.com',
+  projectId: 'gdg-burnaby',
+  storageBucket: 'gdg-burnaby.appspot.com',
+  messagingSenderId: '638355114414',
+  appId: '1:638355114414:web:633a799364844d6d16e03f',
+  measurementId: 'G-X6CJ1MK1WT',
 });
 export const db = getFirestore(firebaseApp);
